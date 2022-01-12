c8a3cd898639a3e3237e634d258f5f8e6f4782ef
kangbongja1 <71359321+kangbongja1@users.noreply.github.com>
2022-01-12 16:40:18 +0900

    Create SidebarData.js
    pjt Cpnt

---
+++ SidebarData.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import * as BsIcons from 'react-icons/bs';
+export const SidebarData = [
+  {
+    title: 'Home',
+    path: '/Home',
+    icon: <BsIcons.BsPersonBoundingBox />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'My program',
+    path: '/MyProgram',
+    icon: <BsIcons.BsFillHouseDoorFill />,
+    cName: 'nav-text'
+  },
+   {
+    title: '유학생활',
+    path: '/exStd',
+    icon: <BsIcons.BsFillHouseDoorFill />,
+    cName: 'nav-text'
+  },
+   {
+    title: '소모임',
+    path: '/club',
+    icon: <BsIcons.BsFillHouseDoorFill />,
+    cName: 'nav-text'
+  },
+   {
+    title: '마이페이지',
+    path: '/MyPage',
+    icon: <BsIcons.BsFillHouseDoorFill />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'About',
+    path: '/About',
+    icon: <BsIcons.BsFillInfoCircleFill />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Contact Us',
+    path: '/Contact',
+    icon: <BsIcons.BsEnvelopeFill />,
+    cName: 'nav-text'
+  }
+];

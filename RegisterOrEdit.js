452d9be53ddac169eb4ddd7766f604d3b92f6567
kangbongja1 <71359321+kangbongja1@users.noreply.github.com>
2022-01-15 22:10:17 +0900

    Create RegisterOrEdit.js
    ctst ROE f

---
+++ RegisterOrEdit.js
@@ -0,0 +1,16 @@
+// RegisterOrEdit.js 
+import React from "react"; 
+import { Link } from "react-router-dom"; 
+import { Button, Input } from "antd"; 
+
+const { TextArea } = Input; function RegisterOrEdit(props) { return ( <div style={{ maxWidth: "700px", margin: "2rem auto" }}> <Link to="/"> 
+<Button>←</Button> </Link> 
+<form onSubmit> <br /> 
+<div style={{ maxWidth: "700px", margin: "2rem" }}> 
+<label>Title: </label> 
+<Input onChange={props.handleTitleChange} value={props.titleValue} type="text" name="title" /> 
+<hr></hr> <TextArea onChange={props.handleContentChange} value={props.contentValue} name="content" /> 
+</div> <Button type="danger" onClick={props.handleSubmit}> {props.updateRequest ? "수정" : "등록"}
+</Button> </form> </div> ); } 
+
+export default RegisterOrEdit;
